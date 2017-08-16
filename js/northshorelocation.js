f947875a1dc9505b37ac767eff9ddfa867475d0a
Rama Ibrahim <Ramochka2020@users.noreply.github.com>
2017-08-16 07:24:30 -0700

    Add files via upload

---
+++ js/northshorelocation.js
@@ -0,0 +1,78 @@
+//****MODEL****//
+var locations = [{
+    
+  title: 'Park Royal Shopping Centre',
+  url: "www.shopparkroyal.com",
+  lat:  49.3248,
+  lng: -123.1368,
+  
+},
+                
+                    {
+        title: 'Kay Meek Centre ',
+        url: "kaymeek.com",
+        lat: 49.3380, 
+        lng: -123.1602,
+     
+},
+    
+                 
+                 {
+  title: 'Capliano River Regional Park',
+  url: "www.capbridge.com",
+  lat: 49.3523,
+  lng: -123.1141,
+ 
+},
+    
+                 {
+  title: 'Whytecliff Park',
+  url: "whytecliffpark.com",                 
+  lat: 49.3731,
+  lng: -123.2886,
+ 
+},
+            
+{ 
+    title: 'Lonsdale Quay Market',
+    url: "bit.ly/2vy9GsC",
+    lat: 49.3107, 
+    lng: -123.0817
+
+    },
+                 
+                 {
+  title: 'Lynn Canyon',
+                    
+  url: "lynncanyon.ca",
+  lat: 49.3439,
+  lng: -123.0181,
+ 
+},
+                  {
+  title: 'Ambleside Park',
+  url: "bit.ly/2wJLG4h",
+  lat: 49.3253,
+  lng: -123.1491,
+ 
+},
+                 {
+        title: 'Grouse Mountain',
+        url: "http://bit.ly/2vy3oJs",
+        lat: 49.3801, 
+        lng: -123.0815,
+},
+    
+                 {
+  title: 'John Lawson Park',
+  url: "bit.ly/2vybh1E",
+  lat: 49.3273,
+  lng: -123.1597,
+ 
+}, {
+  title: 'Lighthouse Park',
+  url: "www.lighthousepark.ca",
+  lat: 49.3316,
+  lng: -123.2636,
+ 
+},  ];
